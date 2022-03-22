5e2c1c392b2258f9dd008e9d3fb1c11ce99d17b4
zhrw24 <101820723+zhrw24@users.noreply.github.com>
2022-03-22 22:15:04 +0300

    Add files via upload

---
+++ EventsTask/EventsTask/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EventsTask
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Publisher publisher = new Publisher();
+            publisher.KeyPressed += (sender, insertChar) => Console.WriteLine($" Введено: {insertChar}");
+            publisher.Run();
+        }
+    }
+}
---
+++ EventsTask/EventsTask/Publisher.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsTask
+{
+    class Publisher
+    {
+        public event EventHandler<char> KeyPressed;
+
+        public void Run()
+        {
+            
+
+            bool exit = false;
+
+            while(!exit)
+            {
+                char insertChar = Console.ReadKey().KeyChar;
+                if (insertChar == 'c')
+                {
+                    exit = true;
+                    Console.WriteLine(" The end");
+                    break; 
+                }
+                  
+                
+                KeyPressed?.Invoke(this, insertChar);
+            }
+        }
+    }
+}
